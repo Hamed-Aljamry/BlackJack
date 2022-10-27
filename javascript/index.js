9995274138da9beffdf202524dc26ae419c2d8bf
hamed aljamry <hamed970@hotmail.com>
2022-10-27 23:51:59 +0100

    testing javascript

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
+
+function getDeck()
+{
+  let deck = new Array()
+return deck
+}
