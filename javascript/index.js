d0a033540880229d015b2851b79eba873bf5fa39
hamed aljamry <hamed970@hotmail.com>
2022-10-28 00:18:58 +0100

    priting out each card with each suit

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,19 @@
 var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-var deck = new Array();
 
 function getDeck()
 {
   let deck = new Array()
-return deck
+
+  for (let x = 0; x < suits.length; x += 1)
+  {
+    for (let y = 0; y < values.length; y += 1)
+    {
+      let card = { Value: values[y], Suit: suits[x] };
+      deck.push(card);
+    }
+  }
+  return deck;
 }
+
+console.log(getDeck())
