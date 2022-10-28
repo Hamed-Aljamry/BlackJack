4b00a4904117847a6847eac33266c55d53bb9a3f
hamed aljamry <hamed970@hotmail.com>
2022-10-28 15:07:34 +0100

    gave values to(Q,J,K,A) in deck

--- javascript/index.js
+++ javascript/index.js
@@ -5,28 +5,44 @@ var deck = new Array();
 function getDeck()
 {
   let deck = new Array()
-
-  for (let x = 0; x < suits.length; x += 1)
+  for(let x = 0; x < suits.length; x += 1)
   {
-    for (let y = 0; y < values.length; y += 1)
+    for(let y = 0; y < values.length; y += 1)
     {
-      var weight = parseInt(values[x]);
-
-
-
-      let card = { Value: values[y], Suit: suits[x] };
+      var specify = parseInt(values[y]);
+      if (values[y] == "J" || values[y] == "Q" || values[y] == "K")
+        specify = 10;
+      if (values[y] == "A")
+        specify = 11;
+      var card = { Value: values[y], Suit: suits[x], Weight: specify };
       deck.push(card);
     }
   }
+  // return specify;
   return deck;
 }
-
-// console.log(getDeck())
-
-
-var test = ["hi", "32", "92"]
-
-console.log(parseInt(test[1]))
+console.log(getDeck())
 
 
-console.log(parseInt(test[0]))
+
+
+
+
+
+// console.log(parseInt(values[0]));
+
+
+// console.log(suits.length)
+// console.log(values.length)
+
+// var weight = parseInt(values[x]);
+// var x = 0
+// x++
+// console.log(parseInt(values[x]))
+
+// var test = ["hi", "32", "92"]
+
+// console.log(parseInt(test[1]))
+
+
+// console.log(parseInt(test[0]))
