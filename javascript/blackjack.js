884a1e29930b66229ddc75680b54f117d3e431dc
hamed aljamry <hamed970@hotmail.com>
2022-10-30 23:19:29 +0000

    conditional statement on value of card for the number to be printed out

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -14,6 +14,7 @@ var canHit = true; // allows me to draw cards while yourSum <= 21
 window.onload = function(){
   buildDeck();
   shuffleDeck();
+  startGame();
 }
 
 
@@ -36,9 +37,27 @@ function buildDeck(){
 function shuffleDeck(){
   for(let i = 0; i < deck.length; i += 1){
     let j = Math.floor(Math.random() * deck.length); // mathrandom takes num from 0-1, then mulitpy by 52, i will get a number from 0-51.9999, mathfloor will help me get it as an integer.
-    let temp = deck[i]
+    let temp = deck[i] // swapping the two postions/ indices
     deck[i] = deck[j]
     deck[j] = temp;
   }
   console.log(deck);
+}
+
+function startGame(){
+  hidden = deck.pop();// taking the last card on the deck
+  dealerSum += getValue(hidden)
+}
+
+function getValue(card){
+  let data = card.split("-"); // example, random card: "4-C" should be like -> ["4", "C"]
+  let value = data[0]; //getting the number now, since the letter has been split in teh array.
+
+  if (isNaN(value)) { // if its not a number value then..
+    if (value == "A") { //if its A we return 11
+      return 11;
+    }
+    return 10;// if its anything other than A which in this case is J,Q,K then we return 10
+  }
+  return parseInt(value); //otherwise we just return whatever number the value is, we use parseInt to change from string to integer
 }
