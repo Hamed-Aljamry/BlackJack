517195fcd154cb4b40aeb8384d1cd72fe57533a4
hamed aljamry <hamed970@hotmail.com>
2022-10-30 22:09:02 +0000

    javascript variables and deck function

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -8,7 +8,7 @@ var yourAceCount = 0;
 var hidden;
 var deck;
 
-var canHit = true; // allows player (you) to draw while yourSum <= 21
+var canHit = true; // allows me to draw cards while yourSum <= 21
 
 
 window.onload = function(){
@@ -17,5 +17,13 @@ window.onload = function(){
 
 
 function buildDeck(){
-  let values
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  let types = ["C", "D", "H", "S"];
+  deck = [];
+
+  for (let i = 0; i < types.length; i += 1)//this will loop through each type/suit of card
+  {
+    for(let j = 0; j < values.length; j += 1)// then each suit will loop through each value as it is nested
+      deck.push(values[j] + "-" + types[i]);// this should push cards like "A-C" -> "K-C","A-D" -> "K-D" and so on....
+  }
 }
